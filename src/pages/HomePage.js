47ec19338506ea59a526c73c33d7226e5d67f1fc
ZaneAyris <108333278+ZaneAyris@users.noreply.github.com>
2023-11-18 20:20:10 +1300

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,20 @@ import React from 'react';
 const HomePage = () => {
   console.log("/")
   return (
-    
-      <img className="MainImage" src="/Leo-Art.github.io/imgs/topdownbug.png" alt="Main" />
+    <>
+    <ul className='DuoElementCentered'>
+      <li className='HomeText'>
+        <h1>LEO</h1>
+        <h1>KOVACS</h1>
+      </li>
+      
+      <li>
+        <img className="MainImage" src="/Leo-Art.github.io/imgs/topdownbug.png" alt="Main" />
+      </li>
+      
+    </ul>
+
+    </>
   );
 };
 
