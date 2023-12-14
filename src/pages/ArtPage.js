486bf51b792c77f90dd30b88f64fb0c5a31f147a
ZaneAyris <108333278+ZaneAyris@users.noreply.github.com>
2023-12-14 15:58:21 +1300

    Added basic Art template

--- src/pages/ArtPage.js
+++ src/pages/ArtPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ArtPage = () => {
   return (
-    <div>
+    <div className='Art'>
       <h1>Art</h1>
       <img className='ArtMainImage' src='/Leo-Art.github.io/imgs/heart breathe glare.gif'/>
     </div>
